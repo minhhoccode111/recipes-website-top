1c5b7faf4314f2c9eefc163581032f281894ae0a
Đặng Hoàng Minh <minhhoccode111@gmail.com>
2023-10-17 19:15:57 +0700

    setup environment to redo project with webpack, npm, tailwindcss

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Set up your README.md file and write a brief introduction describing what the current project is and what skills you will have demonstrated once you have completed it. (You can also do this as a self-reflection at the end of the project, which is a good way to review that you have learned).
 
-[View all projects' live demos](https://minhhoccode111.github.io/all-projects-live-demos/) 
+Second time I try to re-do this simple project after learning Advanced HTML & CSS course, Animation section. This project now use: npm, webpack, tailwindcss, babel, and gh-pages to host.
+
+[View all projects' live demos](https://minhhoccode111.github.io/all-projects-live-demos/)
 
 [Recipes Website TOP](https://minhhoccode111.github.io/recipes-website-top/)
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Recipes Website',
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+    ],
+  },
+};
