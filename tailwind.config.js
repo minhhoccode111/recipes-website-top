beabc6c54b87332cf3048f86a5c2db3fd3b09309
Đặng Hoàng Minh <minhhoccode111@gmail.com>
2023-10-17 21:38:06 +0700

    create header with tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+    },
   },
   plugins: [],
 };
